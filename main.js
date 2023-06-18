daf4fa8a27abdf2f624619fa92082ef6e8e7d541
sabo <124112648+sabo-codes@users.noreply.github.com>
2023-06-17 20:37:47 -0400

    updated main.js & index.html

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ document.querySelector('button').addEventListener('click', apiRequest)
 async function apiRequest(){
     const rapperName = document.querySelector('input').value
     try{
-        const response = await fetch(`https://simple-rapper-api.herokuapp.com/api/${rapperName}`)
+        const response = await fetch(`https://misty-gray-swimsuit.cyclic.app/api/${rapperName}`)
         const data = await response.json()
 
         console.log(data)
